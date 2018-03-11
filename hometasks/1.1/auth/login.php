d6f4eb988163d2117430166c22b3169f45a464a6
Andrii Hnatyshyn <andrii.hnatyshyn@gmail.com>
2018-03-11 23:45:35 +0200

    cookie added

--- hometasks/1.1/auth/login.php
+++ hometasks/1.1/auth/login.php
@@ -1,12 +1,24 @@
 <?php
     session_start();
+    if(count($_POST) > 0){
+        if($_POST['login'] == 'admin' && $_POST['password'] == "qwerty"){
+            $_SESSION['is_auth'] = true;
 
-    if($_POST['login'] == 'admin' && $_POST['password'] == "qwerty"){
-        $_SESSION['is_auth'] = true;
+            if(isset($_POST['remember'])){
+                setcookie('login','admin',time() + 3600 * 24 * 7, '/');
+            }
 
-        header('Location: index.php');
-        exit;
+            header('Location: ../index.php');
+            exit;
+        }
+        else{
+            $msg = "Wrong Login or Password";
+        }
+    }else{
+        $msg = "";
     }
+
+
     ?>
 
 <form method="post">
@@ -14,5 +26,7 @@
     <input type="text" name="login"><br>
     Password: <br>
     <input type="password" name="password"><br>
+    <input type="checkbox" name="remember">Remember me<br>
     <input type="submit" value="Submit">
 </form>
+<p><strong><?= $msg; ?></strong></p>
--- hometasks/1.1/index.php
+++ hometasks/1.1/index.php
@@ -1,22 +1,38 @@
-<a href="auth/login.php">Зайти</a>
-
 <?php
 	//error_reporting(-1);
 	session_start();
 
-	if(isset($_SESSION['is_auth'])){
-        echo "Authorized!!!";
+	if(!$_SESSION['is_auth']){
+        echo "<a href=\"auth/login.php\">Зайти</a>";
+
+        $list = scandir('data');
+
+        foreach($list as $fname){
+            if(is_file("data/$fname")){
+                echo "<p><a href=\"post.php?fname=$fname\">$fname</a><br></p>";
+            }
+        }
+
+	}else{
+        $name = $_POST['login'];
+        echo "You authorized as $name "."<a href=\"auth/exit.php\">Вийти</a>";
+
+        $list = scandir('data');
+
+        foreach($list as $fname){
+            if(is_file("data/$fname")){
+                echo "<p><a href=\"post.php?fname=$fname\">$fname</a><br>"." (<a href=\"edit.php?fname=$fname\">Редагувати</a>)</p>";
+            }
+        }
+
+        echo "<a href=\"add.php\">Добавить</a>";
+
     }
-	$list = scandir('data');
-	
-	foreach($list as $fname){
-		if(is_file("data/$fname")){
-			echo "<p><a href=\"post.php?fname=$fname\">$fname</a><br>"." (<a href=\"edit.php?fname=$fname\">Редагувати</a>)</p>";
-		}
-	}
-	
+
+
+
 ?>
-<a href="add.php">Добавить</a>
+
 
 
 
